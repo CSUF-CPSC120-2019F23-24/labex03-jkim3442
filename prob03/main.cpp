75b82b367b61a1c4558bb41edc0f2152ec9eefef
jkim3442 <jkim3442@cs104u-5g.acad.fullerton.edu>
2019-09-07 13:47:11 -0700

    Wrote program.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,24 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+
+int main()
+{
+  //First we name the program and ask for their height in inches as well as identify our variables.
+
+  int height_in_inches;
+
+  std::cout << "Please enter the person's height in inches: ";
+  std::cin >> height_in_inches;
+
+  //Next we do our calculations.
+
+  int total = height_in_inches / 12;
+  int remainder = height_in_inches % 12;
+
+  //Finally we write out our answer.
+
+  std::cout << "Total height is: " << total << "\'" << remainder << "\".\n";
+
+  return 0;
+}
